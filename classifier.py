b52b3e9abb04383a0f01f2045c60227c6b604c8d
Lance Strait <lancestrait10@hotmail.com>
2018-05-23 13:35:49 -0500

    added classifier skeleton

---
+++ classifier.py
@@ -0,0 +1,10 @@
+'''
+Goal: Build a simple, rules-based classifier that identifies issues that are small, stylistic changes as opposed to larger, more signficant
+algorithm or code changes.
+'''
+
+
+
+def classify_issue(issue):
+	""" returns true if the given issue is a small style/documentation change """
+	
