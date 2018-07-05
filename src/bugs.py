bba0aca64cbca183762b9d647f3088c3da2e9b7d
Lance Strait <lancestrait10@hotmail.com>
2018-07-05 15:45:58 -0500

    more work on scraper

--- src/bugs.py
+++ src/bugs.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
 
-for bug_id in range(1,2):
+for bug_id in range(2,3):
 	bug_url = 'https://bugs.eclipse.org/bugs/show_bug.cgi?id=' + str(bug_id)
 	page = urlopen(bug_url)
 	soup = BeautifulSoup(page, 'html.parser')	
@@ -12,13 +12,25 @@ for bug_id in range(1,2):
 	status = soup.select("#static_bug_status")[0].text
 	product = soup.select("#field_container_product")[0].text
 	component = soup.select("#field_container_component")[0].text
+	duplicates = soup.select("#duplicates")[0].select("a")[0]['href']
 	#TODO: select only the date/time here
 	created_at = soup.select("#bz_show_bug_column_2")[0].select("td")[0].text
-	print(component)
+	print(duplicates)
 
 
 	#TODO: scrape the page with the updates, need to find who marked as resolved
 	#TODO: also need to get the date/time it was marked as fixed
 	#TODO: handle duplicates same way as well.
-	history_url = soup.select("#bz_show_bug_column_2")[0].select("td")[1].select("a")[0]['href']
-	print(history_url)
+	bug_history_url = 'https://bugs.eclipse.org/bugs/show_activity.cgi?id=' + str(bug_id)
+	page = urlopen(bug_history_url)
+	soup = BeautifulSoup(page, 'html.parser')	
+	data = []
+	table = soup.find('table', attrs={'id':'bug_activity'})
+	#print(table)
+	table_body = table.find('tbody')
+	rows = table.find_all('tr')
+	for row in rows:
+		cols = row.find_all('td')
+		cols = [ele.text.strip() for ele in cols]
+		data.append([ele for ele in cols if ele]) # Get rid of empty values
+	#print(data)
