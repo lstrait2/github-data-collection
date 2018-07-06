90be0425d1e2aa42a6bb03a8fdce048343653495
Lance Strait <lancestrait10@hotmail.com>
2018-07-06 12:33:23 -0500

    fixed scraper getting bug fixer id

--- src/bugs.py
+++ src/bugs.py
@@ -1,38 +1,60 @@
+import json
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
 
-for bug_id in range(2,3):
+issues = []
+for bug_id in range(1,1000):
+	print("getting issue: #" + str(bug_id))
 	bug_url = 'https://bugs.eclipse.org/bugs/show_bug.cgi?id=' + str(bug_id)
 	page = urlopen(bug_url)
 	soup = BeautifulSoup(page, 'html.parser')	
-
 	short_desc = soup.select("#short_desc_nonedit_display")[0].text
 	long_desc = soup.select(".bz_first_comment")[0].select(".bz_comment_text")[0].text
 	status = soup.select("#static_bug_status")[0].text
 	product = soup.select("#field_container_product")[0].text
 	component = soup.select("#field_container_component")[0].text
 	duplicates = []
-	for duplicate in soup.select("#duplicates")[0].select("a"):
-		duplicates.append(duplicate.text)
+	duplicates_div = soup.select("#duplicates")
+	if duplicates_div:
+		for duplicate in duplicates_div[0].select("a"):
+			duplicates.append(duplicate.text)
 	#TODO: select only the date/time here
 	created_at = soup.select("#bz_show_bug_column_2")[0].select("td")[0].text
-	print(duplicates)
 
-
-	#TODO: scrape the page with the updates, need to find who marked as resolved
-	#TODO: also need to get the date/time it was marked as fixed
-	#TODO: handle duplicates same way as well.
+	# scrape the page with the updates, need to find who marked as fixed and when
 	bug_history_url = 'https://bugs.eclipse.org/bugs/show_activity.cgi?id=' + str(bug_id)
 	page = urlopen(bug_history_url)
 	soup = BeautifulSoup(page, 'html.parser')	
 	data = []
 	table = soup.find('table', attrs={'id':'bug_activity'})
-	#print(table)
 	table_body = table.find('tbody')
 	rows = table.find_all('tr')
 	for row in rows:
 		cols = row.find_all('td')
 		cols = [ele.text.strip() for ele in cols]
 		data.append([ele for ele in cols if ele]) # Get rid of empty values
-	#print(data)
+	#TODO: Two possible ways for the bug to be marked done (make sure this works)
+	completed_by = None
+	completed_time = None
+	for idx, row in enumerate(data):
+		if len(row) > 4 and row[4] == 'RESOLVED' and len(data[idx+1]) > 2 and data[idx+1][2] == 'FIXED':
+			completed_by = row[0]
+			completed_time = row[1]
+			break
+
+	issue = {}
+	issue['issue_id'] = bug_id
+	issue['short_desc'] = short_desc
+	issue['long_desc'] = long_desc
+	issue['component'] = component
+	issue['product'] = product
+	issue['duplicates'] = duplicates
+	issue['completed_by'] = completed_by
+	issue['completed_at'] = completed_time
+	issue['created_at'] = created_at
+	issues.append(issue)
+
+with open('data/eclipse/eclpise_issues1.json', 'w') as f:
+    json.dump(issues, f, indent=4)
+
