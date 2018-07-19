1f18c0852d901cf5d346324d359499b6006301e0
Lance Strait <lancestrait10@hotmail.com>
2018-07-19 08:40:20 -0500

    finished cleaning flutter data

--- src/local_commits.py
+++ src/local_commits.py
@@ -1,4 +1,14 @@
-with open('data/flutter/flutter_issues_prs.json') as json_data:
-    issues_prs = json.load(json_data)
-
+import json
+with open('data/flutter/flutter_issues_prs_temp.json') as json_data:
+	issues_prs = json.load(json_data)
 for issue_pr in issues_prs:
+	remove_commits = []
+	for commit in issue_pr['local_commits']:
+		if commit['author'] not in commit['commit']:
+			issue_pr['master_commits'].append(commit)
+			remove_commits.append(commit)
+	for commit in remove_commits:
+		issue_pr['local_commits'].remove(commit)
+with open('data/flutter/issues_prs_temp.json', 'w') as f:
+    json.dump(issues_prs, f, indent=4)
+
